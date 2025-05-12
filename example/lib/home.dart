23220f30c57130368b8799874655fdb88408ec58
Mushthak VP <mail.musthak@gmail.com>
2025-05-12 17:55:57 +0530

    responsive helper changed

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_helper/responsive.dart';
+import 'package:responsive_helper/responsive_helper.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
